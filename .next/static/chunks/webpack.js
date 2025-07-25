d269f40a0cf65c51b77d2e4175844508c4374c6f
Ali Khan <Ali.Khan@partner.bmwgroup.com>
2025-07-25 17:02:10 +0100

    Remove all nodemailer and contact API references

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2d78b2e51edb0558"; }
+/******/ 		__webpack_require__.h = function() { return "d579f10ba728f138"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
