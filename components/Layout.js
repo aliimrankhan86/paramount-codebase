d70b213d6340a97eb8632238ff8e646a2fe69cbd
Ali Khan <Ali.Khan@partner.bmwgroup.com>
2025-07-25 17:18:27 +0100

    Remove welcome bonus subscribe modal from layout

--- components/Layout.js
+++ components/Layout.js
@@ -4,36 +4,15 @@ import React from "react";
 import Header from "./header";
 import ContactUs from "./support";
 import Footer from "./footer";
-import Subscribe from "./subscribe";
+
 
 export default function Layout({ children }) {
-  const [visible, setVisible] = React.useState(false);
 
-  React.useEffect(() => {
-    const initialVisit = !localStorage.getItem("visited");
-
-    if (initialVisit) {
-      setVisible(true);
-      localStorage.setItem("visited", "true");
-    }
-  }, []);
-
-  // React.useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     setVisible(true);
-  //   }, 1000);
-
-  //   return () => clearTimeout(timer);
-  // }, []);
-
-  const handleClose = () => {
-    setVisible(false);
-  };
 
   return (
     <>
       <Header />
-      {visible && <Subscribe visible={visible} handleClose={handleClose} />}
+
 
       {children}
 
