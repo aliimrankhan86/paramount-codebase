cc13023e4a83cca9c43392ace095e8a46ff6c7c3
Ali Khan <Ali.Khan@partner.bmwgroup.com>
2025-07-25 16:57:39 +0100

    Removed contact.js and related references

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ec886135c0ddb8a1"; }
+/******/ 		__webpack_require__.h = function() { return "2d78b2e51edb0558"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
