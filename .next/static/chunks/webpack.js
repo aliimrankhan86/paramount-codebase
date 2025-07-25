f24be0af70992acce8e44571859bb996459f4cb1
Ali Khan <Ali.Khan@partner.bmwgroup.com>
2025-07-25 16:41:40 +0100

    Install nodemailer for backend email functionality

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e08ff956de002c99"; }
+/******/ 		__webpack_require__.h = function() { return "ec886135c0ddb8a1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
