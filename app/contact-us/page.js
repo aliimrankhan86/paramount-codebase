5fcd9a52467d21f5437ff7f303273144a1ab70eb
Ali Khan <Ali.Khan@partner.bmwgroup.com>
2025-07-28 20:38:26 +0100

    build issue resolve v3

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -3,3 +3,11 @@
 // export default function ContactUs() {
 //   return <ContactUsPage />;
 // }
+
+"use client";
+
+import ContactUsPage from "@/screens/contact-us";
+
+export default function ContactUsPage() {
+  return <div>Contact Us </div>;
+}
